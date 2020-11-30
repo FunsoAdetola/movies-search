df310e4be2005c8f1cfb5a62207875a3e3d800b1
funsoadetola <funsoadetola@gmail.com>
2020-11-30 21:18:23 +0100

    Your commit message

---
+++ build/precache-manifest.470841e7ab68a64ec3a13b373353a27a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5632deb36571d9c288629d2cbf9a6dd1",
+    "url": "/movies-search/index.html"
+  },
+  {
+    "revision": "c61e50eead971f432fb2",
+    "url": "/movies-search/static/css/main.5c98d447.chunk.css"
+  },
+  {
+    "revision": "faf09e39017c9cefc129",
+    "url": "/movies-search/static/js/2.67d4f011.chunk.js"
+  },
+  {
+    "revision": "c61e50eead971f432fb2",
+    "url": "/movies-search/static/js/main.ea4d1f6f.chunk.js"
+  },
+  {
+    "revision": "c61c98ef15a31ad07faa",
+    "url": "/movies-search/static/js/runtime~main.f0fab22e.js"
+  }
+]);
